e1b92d99c476389ca67412dae57513f422108afe
Desteveco <destevezcounago@gmail.com>
2023-12-11 02:45:12 +0100

    final

--- src/play_random_music.py
+++ src/play_random_music.py
@@ -2,8 +2,3 @@ import subprocess
 def vlc_opener(playlist, ruta_vlc):
     comando_vlc = [ruta_vlc] + playlist
     subprocess.Popen(comando_vlc)
-
-import subprocess
-def vlc_opener(playlist, ruta_vlc):
-    comando_vlc = [ruta_vlc] + playlist
-    subprocess.Popen(comando_vlc)
